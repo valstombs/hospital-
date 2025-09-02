a6b127066a6e2cfe87abd87171afd78c0a653034
valstombs <callejasvalerie8@gmail.com>
2025-09-02 14:25:14 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "hospital-nsa-cache-v1";
+const urlsToCache = ["index.html", "manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
